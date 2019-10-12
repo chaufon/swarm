af9d8db741fbb5ce3d0bc179d989f4a008eab0bc
chaufon <laurentemurga@gmail.com>
2019-10-12 17:48:03 -0500

    Update install_compose.sh

--- install_compose.sh
+++ install_compose.sh
@@ -1,2 +1,7 @@
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
+mkdir wordpress-project
+cd wordpress-project
+mkdir wordpress
+mkdir mysql
+
