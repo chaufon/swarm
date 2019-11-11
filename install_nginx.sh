c384f8ad97176552b7cbc69d20f6d09f27f4c873
chaufon <laurentemurga@gmail.com>
2019-11-11 00:22:29 -0500

    Create install_nginx.sh

---
+++ install_nginx.sh
@@ -0,0 +1,26 @@
+NODES
+apt-get update -y && apt-get install -y nginx
+vim /var/ww/html/index.nginx-debian.html  (colocar identificador de cada servidor)
+systemctl restart nginx
+systemctl enable nginx
+
+PROXY
+apt-get update -y && apt-get install -y nginx
+vim /etc/nginx/sites-available/default
+upstream backend  {
+  server 157.245.212.27;
+  server 167.99.55.87;
+}
+
+server {
+ listen 80;
+ location / {
+   proxy_pass http://backend;
+  }
+}
+
+systemctl restart nginx
+systemctl enable nginx
+
+
+tail -f /var/log/nginx/access.log
